4a401a705a93583c1a757642449e31e1480336dc
lisswart <arnoudlisa@gmail.com>
2021-07-18 19:01:25 -0400

    done

--- lib/student.rb
+++ lib/student.rb
@@ -2,5 +2,52 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade, id=nil)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    # saves an instance of the Student class to the database
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
   
+  def self.create(attributes)
+    # takes in a hash of attributes and uses metaprogramming to create a new student object.  
+    student = Student.new(name, grade)
+
+    # then it uses the #save method to save that student to the database
+    student.save
+
+    # returns a new object that it instantiated
+    student
+  end
+
 end
